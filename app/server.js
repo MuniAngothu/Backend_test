f1508a5b742d0c1c7eaae3013143e79fd0704c4f
MuniAngothu <angothuramudu@gmail.com>
2025-07-27 12:13:16 +0530

    added workflow and docker image

--- app/server.js
+++ app/server.js
@@ -15,3 +15,8 @@ app.use('/orders', orderRoutes);
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
+
+
+// app.listen(PORT, '0.0.0.0', () => {
+//   console.log(`🚀 Server is running on http://0.0.0.0:${PORT}`);
+// });
